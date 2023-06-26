a68575d33aad2bdc9b2494b23d5e182229b0c64e
lRebornsl <anthony_vasquez98@hotmail.com>
2023-06-26 16:38:49 -0500

    Add HTML and SCSS content for footer

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './index.scss'
+import footer from "./modules/footer.js";
+
+footer();
---
+++ src/modules/footer.js
@@ -0,0 +1,12 @@
+const footer = () => {
+  const footer = document.createElement('footer');
+  const container = document.createElement('div');
+
+  footer.classList.add('footer','p-fix');
+  container.classList.add('container');
+  container.innerHTML = '<p>Created By Anthony Vásquez and Andrés Zamorano CC License</p>';
+  footer.appendChild(container);
+  document.body.appendChild(footer);
+}
+
+export default footer;
